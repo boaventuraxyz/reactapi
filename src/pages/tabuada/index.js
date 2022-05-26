077ce95199277003c026de246a417303d68a765a
Victor Martins Fonseca <victor.fonseca0@gmail.com>
2022-05-25 21:25:29 -0300

    tabuada feito

--- src/pages/tabuada/index.js
+++ src/pages/tabuada/index.js
@@ -8,14 +8,16 @@ export default function Index(){
 
 
     async function calcular(){
-        const resp = await axios.get('http://localhost:5000/tabuada' + n)
+        const resp = await axios.get('http://localhost:5000/tabuada?n=' + n)
         setresp(resp.data.tabuada)
     }
     return(
         <main>
             <h1>tabuada</h1>
             numero: <input type='text' value={n} onChange={e => setn(Number(e.target.value))} />
+            <div>
             <button onClick={calcular}> calcular</button>
+            </div>
             {resposta}
         </main>
     )
